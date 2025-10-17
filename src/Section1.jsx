5c4ca22892444ff8f881a122e78de05ddb3d2e2e
Akaki Gegechkori <akagege87@gmail.com>
2025-10-18 00:21:32 +0400

    Lecture-12

---
+++ src/Section1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <h1 className='section1'>Section1</h1>
+  )
+}
+
+export default Section1
---
+++ src/Section3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section3 = () => {
+  return (
+    <h1 className='section3'>Section3</h1>
+  )
+}
+
+export default Section3
